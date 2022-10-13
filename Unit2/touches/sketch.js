e594cfa6e9c4e6ad73b94972cd2e2a60f1c16ffe
Zander <zckarp@ilstu.edu>
2022-10-13 10:21:04 -0500

    touches

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,7 +1,12 @@
 let numberOfTouches ;
+let i1, i2, i3, i4;
 
 function setup() {
   createCanvas(400, 400);
+  i1 = loadImage("images/onecat.jpeg");
+  i2 = loadImage("images/twocat.jpeg");
+  i3 = loadImage("images/redcat.jpg");
+  i4 = loadImage("images/tom.jpg");
 }
 
 function draw() {
@@ -14,25 +19,25 @@ function draw() {
       text("no cat", 5, 22) ; 
       break ;
       
-      case 1: 
-       text("One cat", 5, 22) ; 
-      // put a picture here
+     case 1: 
+      text("One cat", 5, 22) ; 
+      image(i1, 50, 50, 300, 350);
       break ;
       
       case 2:
       text("Two cat", 5, 22) ; 
-            // put a picture here
+      image(i2, 50, 50, 300, 350);
       break ;
       
       case 3:
-     text("Red cat", 5, 22) ; 
-            // put a picture here
+      text("Red cat", 5, 22) ; 
+      image(i3, 50, 50, 300, 350);
       break ;
     
       case 4:
-        text("Blue cat", 5, 22) ; 
-               // put a picture here
-         break ;
+      text("Blue cat", 5, 22) ; 
+      image(i4, 50, 50, 300, 350);
+      break ;
   }
   
 }
