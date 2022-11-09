d77e2fdc62640572a3e5e830924e81d6df761631
Zander <zckarp@ilstu.edu>
2022-11-09 15:42:41 -0600

    Update sketch.js

--- Unit3/particles/sketch.js
+++ Unit3/particles/sketch.js
@@ -1,20 +1,24 @@
-let myCar;
+let cars = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Spawn one object
-  myCar = new Car();
-
+  // for (let i = 0; i < 20; i++) {
+  // cars.push(new Car());
+  // }
+  noStroke();
 }
 
 function draw() {
   background(100);
-
-  myCar.display();
-  myCar.move();
-  fill('white') ;
-  text(myCar.x, 100, 100 ) ;
+  cars.push(new Car());
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].move();
+    if (cars[i].a <= 0) {
+      cars.splice(i, 1);
+    }
+    }
 
 }
 
@@ -25,19 +29,28 @@ class Car {
 
   // constructor
   constructor() {
-    this.x = 100 ;  // initialize your attributes here
-    
+    this.pos = createVector(width/2, height-80) ;  // initialize your attributes here
+    this.vel = createVector(random(-.8, .8), random(-10,-5)) ;
+    this.r = 255;
+    this.g = 255;
+    this.b = 255;
+    this.a = random(200, 255);
   }
 
   // methods
 
   display() {
-    rect(this.x, 100, 75, 25);
+    fill(this.r, this.g, this.b, this.a);
+    ellipse(this.pos.x, this.pos.y, 75, 25);
   }
 
   move() {
-    this.x = this.x + 10;
-    if (this.x > width) this.x = 0 ;
+   this.pos.add(this.vel);
+   this.a = this.a - 5;
+  //  if (this.pos.x > width) this.pos.x = 0;
+  //  if (this.pos.x < 0) this.pos.x = width;
+  //  if (this.pos.y > height) this.pos.y = 0;
+  //  if (this.pos.y < 0) this.pos.y = height;
   }
   
 }
