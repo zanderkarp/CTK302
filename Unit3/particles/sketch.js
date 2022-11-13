eed02bfc53f67ad3da514fbb77c1bde11cc16a1f
Zander <zckarp@ilstu.edu>
2022-11-12 23:04:15 -0600

    unit updates

--- Unit3/particles/sketch.js
+++ Unit3/particles/sketch.js
@@ -1,16 +1,17 @@
 let cars = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
 
   // for (let i = 0; i < 20; i++) {
   // cars.push(new Car());
   // }
   noStroke();
+
 }
 
 function draw() {
-  background(100);
+  background(150);
   cars.push(new Car());
   for (let i = 0; i < cars.length; i++) {
     cars[i].display();
@@ -19,7 +20,26 @@ function draw() {
       cars.splice(i, 1);
     }
     }
+//WHALE
+fill("black");
+ellipse(width/2, height-10, 400, 100);
+triangle(160, 370, 280, 450, 180, 450);
+triangle(35, 440, 35, 505, 75, 505);
+fill("white");
+ellipse(340, 475, 100, 30);
+triangle(144, 447, 180, 444, 185, 490);
+fill("black");
+circle(360, 475, 15, 15);
+fill("white");
+circle(365, 475, 5, 5);
+fill("lightblue");
+rect(0, 490, 500, 10);
+fill("yellow");
+circle(500, 20, 200, 200);
 
+fill("black");
+textSize(32);
+text("Ollie the Orca\ncame up from\nhis home to say \nhello!", 40, 50);
 }
 
 
@@ -29,7 +49,7 @@ class Car {
 
   // constructor
   constructor() {
-    this.pos = createVector(width/2, height-80) ;  // initialize your attributes here
+    this.pos = createVector(325, 435) ;  // initialize your attributes here
     this.vel = createVector(random(-.8, .8), random(-10,-5)) ;
     this.r = 255;
     this.g = 255;
@@ -41,7 +61,7 @@ class Car {
 
   display() {
     fill(this.r, this.g, this.b, this.a);
-    ellipse(this.pos.x, this.pos.y, 75, 25);
+    ellipse(this.pos.x, this.pos.y, 30);
   }
 
   move() {
